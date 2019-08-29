4ff6487b085f78ef29e35741a84ab9625aebd013
Jorge Valdez <jrvaldez1974@gmail.com>
2019-08-29 16:34:22 -0500

    Door component created.

---
+++ Source/BuildingEscape1/OpenDoor.cpp
@@ -0,0 +1,43 @@
+// No Rights Reserved. 
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+#include "Math/Rotator.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Get Owner
+	Owner = GetOwner();
+
+	// Create Rotator
+	NewRotation = FRotator(0.f, 90.f, 0.f);
+
+	// Set Rotation on Door
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/BuildingEscape1/OpenDoor.h
@@ -0,0 +1,33 @@
+// No Rights Reserved. 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BUILDINGESCAPE1_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	AActor* Owner;
+
+	FRotator NewRotation;
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+};
